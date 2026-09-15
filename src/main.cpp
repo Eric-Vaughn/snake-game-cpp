30f68d142efab92c28c9d7bf4dbc691715a71edc
oraclemoss <vaughneric4@gmail.com>
2026-09-15 14:58:33 -0500

    include <deque> & create empty Snake class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h>
+#include <deque>
 
 using namespace std; // Tutorial has this, I know it's not best practice
 
@@ -8,6 +9,10 @@ Color darkGreen = {43, 51, 24, 255};
 
 int cellSize = 30;
 int cellCount = 25;
+
+class Snake {
+    
+};
 
 class Food {
     public:
