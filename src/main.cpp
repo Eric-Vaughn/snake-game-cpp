96bc0e86f636e2949725939abfb557736e35ec62
oraclemoss <vaughneric4@gmail.com>
2026-09-15 16:21:57 -0500

    add Reset() to Snake class

--- src/main.cpp
+++ src/main.cpp
@@ -69,6 +69,12 @@ public:
             body.pop_back();
         }
     }
+
+    void Reset()
+    {
+        body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+        direction = {1, 0};
+    }
 };
 
 class Food
@@ -159,7 +165,7 @@ public:
 
     void GameOver()
     {
-        cout << "Game Over" << endl;
+        snake.Reset();
     }
 };
 
